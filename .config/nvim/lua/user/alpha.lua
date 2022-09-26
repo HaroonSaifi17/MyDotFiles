11fa378ca131046c5b7dd4eebd63b7a062438644
Mohd Haroon <haroonsaifi2001@gmail.com>
2022-09-26 15:43:40 +0530

    some twicks

--- .config/nvim/lua/user/alpha.lua
+++ .config/nvim/lua/user/alpha.lua
@@ -22,7 +22,7 @@ dashboard.section.buttons.val = {
   dashboard.button("q", " " .. " Quit", ":qa<CR>"),
 }
 local function footer()
-  return "chrisatmachine.com"
+  return "haroonsaifi2001@gmail.com"
 end
 
 dashboard.section.footer.val = footer()
