09bc222b240f9dfb75804cc091e5257bb2d2b857
Mohd Haroon <haroonsaifi2001@gmail.com>
2023-02-21 20:01:34 +0530

    some twicks

--- .config/nvim/lua/user/nvim-tree.lua
+++ .config/nvim/lua/user/nvim-tree.lua
@@ -71,3 +71,14 @@ nvim_tree.setup {
     },
   },
 }
+local function open_nvim_tree(data)
+  -- buffer is a directory
+  local directory = vim.fn.isdirectory(data.file) == 1
+
+  if not directory then
+    return
+  end
+  -- open the tree
+  require("nvim-tree.api").tree.open()
+end
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
