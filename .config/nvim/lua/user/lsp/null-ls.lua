52fe5ad805ccde5d92b5e7b8b29b17c05cd15d8c
Mohd Haroon <haroonsaifi2001@gmail.com>
2023-08-12 14:04:25 +0530

    some twicks

--- .config/nvim/lua/user/lsp/null-ls.lua
+++ .config/nvim/lua/user/lsp/null-ls.lua
@@ -19,7 +19,7 @@ null_ls.setup({
 
   on_attach = function(client, bufnr)
     if client.server_capabilities.documentFormattingProvider then
-      vim.cmd("nnoremap <silent><buffer> <Leader>f :lua vim.lsp.buf.formatting()<CR>")
+      vim.cmd("nnoremap <silent><buffer> <Leader>f :lua vim.lsp.buf.format({async= true})<CR>")
 
       -- format on save
     --  vim.cmd("autocmd BufWritePost <buffer> lua vim.lsp.buf.formatting()")
