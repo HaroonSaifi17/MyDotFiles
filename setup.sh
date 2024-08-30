aa92fd23fcc7307752945d2bfa7d76b1976c936d
Haroon Saifi <haroonsaifi2001@gmail.com>
2024-08-30 16:40:07 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-PACKAGES="tmux neovim zsh curl wget unzip openssh ripgrep nvm clang python rust tree file git fastfetch go python-black rust-analyzer htop gh stylua lua-language-server "
+PACKAGES="tmux neovim zsh curl wget unzip openssh ripgrep clang python rust tree file git fastfetch go python-black rust-analyzer htop github-cli stylua lua-language-server "
 NPM_PACKAGES="eslint prettier typescript-language-server typescript eslint_d emmet-ls @angular/language-server @angular/cli vscode-langservers-extracted"
 ZSH_PLUGINS="zsh-autosuggestions zsh-syntax-highlighting zsh-history-substring-search"
 current_dir=$(pwd)
@@ -8,6 +8,7 @@ current_dir=$(pwd)
 install_packages() {
     sudo pacman -Syu --noconfirm
     sudo pacman -S --noconfirm --needed $PACKAGES
+    wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
     nvm install --lts
     nvm use --lts
     sudo npm install -g $NPM_PACKAGES
