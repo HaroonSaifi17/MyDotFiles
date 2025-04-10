ddb41a4831c9b2ad786fdf88864396163551e5c2
Haroon Saifi <haroonsaifi2001@gmail.com>
2025-04-10 19:22:05 +0530

    update

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,21 +1,18 @@
 return {
   {
-    "ChristianChiarulli/defaultplus",
-    lazy = false,
+    "ChrlistianChiarulli/defaultplus",
     config = function()
       vim.cmd.colorscheme("defaultplus")
       vim.api.nvim_set_hl(0, "SnacksIndent", { fg = "#333333", nocombine = true })
       vim.api.nvim_set_hl(0, "BlinkCmpMenuBorder", { fg = "#333333", nocombine = true })
-      vim.api.nvim_set_hl(0, "BlinkCmpScrollBarThumb", { bg = "#666666", nocombine = true })
-      vim.api.nvim_set_hl(0, "BlinkCmpGhostText", { fg = "#555555", bg = "#1e1e1e", nocombine = true })
-      vim.api.nvim_set_hl(0, "BlinkCmpMenuSelection", { bg = "#333333", nocombine = true })
-
-      vim.api.nvim_set_hl(0, "Delimiter", { fg = "#7ccdfd", bg = "NONE" })
-      vim.api.nvim_set_hl(0, "Variable", { fg = "#ffffff", bg = "NONE" })
-      vim.api.nvim_set_hl(0, "Function", { fg = "#a6dbff", bg = "NONE", italic = true })
+      vim.api.nvim_set_hl(0, "BlinkCmpScrollBarThumb", { bg = "#444444", nocombine = true })
+      vim.api.nvim_set_hl(0, "BlinkCmpGhostText", { fg = "#999999", nocombine = true })
     end,
   },
   {
     "decaycs/decay.nvim",
+  },
+  {
+    "rebelot/kanagawa.nvim",
   },
 }
